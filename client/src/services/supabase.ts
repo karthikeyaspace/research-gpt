15a0805e198eef2f409abc01f3a3d7614b243309
Karthikeya <veruturikarthikeya@gmail.com>
2024-08-01 07:41:04 +0530

    .

--- client/src/services/supabase.ts
+++ client/src/services/supabase.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://txpjupgxqfaksnmjvpeq.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4cGp1cGd4cWZha3NubWp2cGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA1ODE4MjcsImV4cCI6MjAzNjE1NzgyN30.if0I2fWt5DGYIinpy2RixGZPN5ZEdfPZLr9MWKSF1zg";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
-if (!supabaseKey) {
+if (!supabaseKey) 
   throw new Error("SUPABASE_KEY is not defined");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
