d4876044d5ac2ba2d8bae5542bc7410bf1f14333
Karthikeya <veruturikarthikeya@gmail.com>
2024-08-04 09:50:26 +0530

    vercel error

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,6 +22,16 @@ app.use(cors(corsOptions))
 //routes
 app.use('/ai', airouter)
 app.use('/sources', sourcesrouter)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 
 app.get('/', (req, res) => {
